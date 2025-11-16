7c8423c97025740596db3fb4aac42204875a6d2f
Lukas <contact@lukasbreuer.de>
2025-11-16 13:43:00 +0100

    Implement ErrorLayout

---
+++ src/pages/error/components/ErrorLayout.tsx
@@ -0,0 +1,59 @@
+import MotionContainer from "@/components/animate/motion-container";
+import {varBounce} from "@/components/animate/variants/bounce";
+import {Button} from "@/ui/button";
+import {m} from "motion/react";
+import type {ReactNode} from "react";
+import {Helmet} from "react-helmet";
+import {NavLink} from "react-router";
+
+interface ErrorLayoutProps {
+  title: string;
+  description?: ReactNode;
+  buttonText?: string;
+  slots?: {
+    footer?: ReactNode;
+  };
+}
+
+export default function ErrorLayout({
+                                      title,
+                                      description,
+                                      buttonText = "Go to Home",
+                                      slots = {},
+                                    }: ErrorLayoutProps) {
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <div
+        className="m-auto max-w-[400px] flex items-center justify-center h-full">
+        <MotionContainer
+          className="flex flex-col items-center justify-center px-2 w-full gap-2">
+          <m.div variants={varBounce().in}>
+            <h2 className="text-center">
+              {title}
+            </h2>
+          </m.div>
+          {description && (
+            <m.div variants={varBounce().in}>
+              <p className="text-center text-secondary">
+                {description}
+              </p>
+            </m.div>
+          )}
+
+          {slots.footer ? (
+            slots.footer
+          ) : (
+            <NavLink to={"/"} className="mt-4 w-full flex justify-center">
+              <Button size="lg" variant="contrast">
+                {buttonText}
+              </Button>
+            </NavLink>
+          )}
+        </MotionContainer>
+      </div>
+    </>
+  );
+}
