52a9c90288ea7225b8e386d639ec0ad770c64a49
Lukas <contact@lukasbreuer.de>
2025-11-16 13:40:46 +0100

    Implement App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {Helmet} from "react-helmet";
+import { MotionLazy } from "./components/animate/motion-lazy";
+import { RouteLoading } from "./components/loading";
+
+if (import.meta.env.DEV) {
+  import("react-scan").then(({scan}) => {
+    scan({
+      enabled: false,
+      showToolbar: true,
+      log: false,
+      animationSpeed: "fast",
+    });
+  });
+}
+
+function App({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <Helmet>
+        <link rel="icon" sizes="64x64" href="favicon.ico"/>
+        <title>PodDeck</title>
+      </Helmet>
+      <RouteLoading />
+      <MotionLazy>{children}</MotionLazy>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
