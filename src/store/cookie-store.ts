0b71f96005012a4a287d608103209dcc7c642eae
Lukas <contact@lukasbreuer.de>
2025-11-18 16:10:22 +0100

    Implement cookie-store

---
+++ src/store/cookie-store.ts
@@ -0,0 +1,16 @@
+import type {StateStorage} from 'zustand/middleware';
+import { getCookie, setCookie, removeCookie } from 'typescript-cookie';
+
+const cookiesStorage: StateStorage = {
+  getItem: (name: string) => {
+    return getCookie(name) ?? null;
+  },
+  setItem: (name: string, value: string) => {
+    setCookie(name, value, { expires: 1, secure: true });
+  },
+  removeItem: (name: string) => {
+    removeCookie(name);
+  }
+}
+
+export default cookiesStorage;
