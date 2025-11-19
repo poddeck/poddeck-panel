553252734b7361d5fcec82d06d1063e625dbb7c2
Lukas <contact@lukasbreuer.de>
2025-11-19 19:40:05 +0100

    Update line-loading

--- src/components/loading/line-loading.tsx
+++ src/components/loading/line-loading.tsx
@@ -1,14 +1,19 @@
 import "./line-loading.css";
+import {useTheme} from 'next-themes';
+import {cn} from "@/lib/utils"
+
 
 export function LineLoading() {
+  const {theme} = useTheme();
+
   return (
     <div
       className="flex h-full min-h-screen w-full flex-col items-center justify-center">
       <div
-        className="relative h-1.5 w-96 overflow-hidden rounded bg-gray-500"
+        className="relative h-1.5 w-96 overflow-hidden rounded bg-gray-500!"
       >
         <div
-          className="absolute left-0 top-0 h-full w-1/3 animate-loading bg-black"
+          className={cn("absolute left-0 top-0 h-full w-1/3 animate-loading", theme === "light" ? "bg-black!" : "bg-white!")}
         />
       </div>
     </div>
