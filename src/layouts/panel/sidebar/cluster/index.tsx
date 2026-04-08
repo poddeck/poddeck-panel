4059237261f318baef5c8ce925e43238c327fb79
mcz <miguel.cortina.zapp@gmail.com>
2026-04-08 23:07:54 +0200

    Fix cluster ID not persisted to store on initial load

--- src/layouts/panel/sidebar/cluster/index.tsx
+++ src/layouts/panel/sidebar/cluster/index.tsx
@@ -61,6 +61,7 @@ export function SidebarClusterSwitcher() {
         return;
       }
       setActiveCluster(clusters[0]);
+      setClusterId(clusters[0].id);
       return;
     }
     if (!loading && (!clusters || clusters.length === 0)) {
