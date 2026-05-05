284c761d20195a44005fb87321f94cf7568010ae
mcz <miguel.cortina.zapp@gmail.com>
2026-05-05 20:10:58 +0200

    Scope auth cookies to root path with SameSite=Lax

--- src/store/cookie-store.ts
+++ src/store/cookie-store.ts
@@ -4,9 +4,9 @@ import {getCookie, removeCookie, setCookie} from 'typescript-cookie';
 export const cookieStorage = (expiry: number): StateStorage => ({
   getItem: (name) => getCookie(name) ?? null,
   setItem: (name, value) => {
-    setCookie(name, value, {expires: expiry, secure: true});
+    setCookie(name, value, {expires: expiry, secure: true, path: "/", sameSite: "lax"});
   },
   removeItem: (name) => {
-    removeCookie(name);
+    removeCookie(name, {path: "/", sameSite: "lax"});
   },
 });
